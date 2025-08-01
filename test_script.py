478e44f39f54708407ea014814a84419b5bf26bc
Teo <teo.almasanu@gmail.com>
2025-08-01 13:55:41 +0300

    Redesign python-ci with less tests

--- test_script.py
+++ test_script.py
@@ -1,5 +1,5 @@
 from script import check_status
-from site_pinger import check_status
+from site_pinger import check_site_status
 
 def test_check_status():
     assert check_status() == "OK"
@@ -7,5 +7,5 @@ def test_check_status():
 # Down site example: http://imgbb.com/
 # Up site example: https://www.google.com/
 def test_check_status_success():
-    result = check_status("https://www.google.com/")
+    result = check_site_status("https://www.google.com/")
     assert result == "UP"
