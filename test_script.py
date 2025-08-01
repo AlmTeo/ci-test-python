8d7f0dd7178286fadbd8a223a647c4c40a4b48a8
Teo <teo.almasanu@gmail.com>
2025-08-01 11:01:18 +0300

    Simulate test failure

--- test_script.py
+++ test_script.py
@@ -2,3 +2,6 @@ from script import check_status
 
 def test_check_status():
     assert check_status() == "OK"
+
+def test_check_status_fails():
+    assert check_status() == "FAIL"  # purposefully incorrect to demonstrate failure
